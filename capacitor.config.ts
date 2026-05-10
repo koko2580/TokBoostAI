ff191a3fbe5dad050c059459d69aae159654d4b6
koko2580 <113718870+koko2580@users.noreply.github.com>
2026-05-10 22:30:00 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tokboost.ai',
+  appName: 'TokBoost AI',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
